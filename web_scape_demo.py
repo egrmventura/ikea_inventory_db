ed54c0e7331261675dad11ef2b5bee6a6acb8e9c
Matthew Ventura <59519729+egrmventura@users.noreply.github.com>
2024-01-21 22:03:48 -0500

    py update with notes on bs4 usage for IKEA prod_id finding

--- web_scape_demo.py
+++ web_scape_demo.py
@@ -2,11 +2,14 @@ from bs4 import BeautifulSoup
 import requests
 
 url2 = "https://www.newegg.com/lacie-model-stfr5000800-5tb/p/2WA-0009-000F0?Item=2WA-0009-000F0&SoldByNewegg=1"
-results = requests.get(url2)
+url3 = "https://www.ikea.com/us/en/p/uppland-sectional-4-seat-corner-blekinge-white-s49384110/"
+results = requests.get(url3)
+
 
 with open(results, 'r') as f:
     contents =  f.read()
     soup = BeautifulSoup(contents, "html.parser")
+
 
     for child in soup.descendants:
         if child.name:
